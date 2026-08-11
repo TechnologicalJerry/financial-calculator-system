3172d4d5bf4e814a24d1d6971acc27ec7c2be5ad
Rajat Verma <cserajatverma@gmail.com>
2026-08-11 20:23:22 +0530

    Add index.ts to export services and routes for identity service

---
+++ express-server/apps/identity-service/src/index.ts
@@ -0,0 +1,6 @@
+export * from './services/auth.service.js';
+export * from './services/financial-profile.service.js';
+export * from './services/financial-account.service.js';
+export * from './routes/auth.router.js';
+export * from './routes/profile.router.js';
+export * from './routes/accounts.router.js';
