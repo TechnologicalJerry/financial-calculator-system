9226e91a2c6858c2c4ab92655cdf917988e58022
Rajat Verma <cserajatverma@gmail.com>
2026-08-11 20:24:36 +0530

    Add index.ts for analytics package to export types, schemas, repositories, services, and routes

---
+++ express-server/packages/analytics/src/index.ts
@@ -0,0 +1,6 @@
+export * from './types/analytics.types.js';
+export * from './schemas/analytics.schemas.js';
+export * from './repositories/report.repository.js';
+export * from './services/analytics.service.js';
+export * from './services/report.service.js';
+export * from './routes/analytics.router.js';
