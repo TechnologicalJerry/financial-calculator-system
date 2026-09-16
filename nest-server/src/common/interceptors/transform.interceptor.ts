48738779c8f26e4b0236202db9256d26cb62fb6f
Rajat Verma <cserajatverma@gmail.com>
2026-09-16 18:07:28 +0530

    feat(nest-common): add transform interceptor

---
+++ nest-server/src/common/interceptors/transform.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponse } from '../dto/api-response.dto';
+
+@Injectable()
+export class TransformInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map(data => {
+        if (data && typeof data === 'object' && 'success' in data && 'timestamp' in data) {
+          return data;
+        }
+        return ApiResponse.ok(data);
+      }),
+    );
+  }
+}
